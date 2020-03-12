bd4576e47d2e5eb253fb43fcdaa6f7ffb664a816
Cgrain <whatarey0206@qq.com>
2020-03-12 11:59:42 +0800

    增加文件夹

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app"> 
-    <h1><a href="https://www.bilibili.com/video/av89760569?p=147">使用 GIt</a></h1> 
+    <h1><a href="https://www.bilibili.com/video/av89760569?p=147">Vue 项目与Git 绑定 </a></h1> 
+  
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
+ 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    
   }
 }
 </script>
---
+++ src/views/home/home.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+  <div>
+    <!--  Hello Vue Ri -->
+
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  //   mounted: {},
+
+  methods: {}
+};
+</script>
+<style>
+</style>
